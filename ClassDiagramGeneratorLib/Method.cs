ef14a7f521b9c5875f735a8e30b6d486d4984d60
Alexander <vyshky2@gmail.com>
2021-10-06 16:45:42 +0400

    Init Method.cs

--- ClassDiagramGeneratorLib/Method.cs
+++ ClassDiagramGeneratorLib/Method.cs
@@ -2,6 +2,14 @@
 {
     public class Method
     {
-        
+        public string Parenthesis { get; set; }
+
+        public string NameMethod { get; set; }
+
+        public Method()
+        {
+            Parenthesis = null;
+            NameMethod = null;
+        }
     }
 }
